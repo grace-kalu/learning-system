cb74bbab565b3c79c8d9fb5b20c093447a4ee361
Meralds-World <grace.chidumaga@gmail.com>
2020-03-16 10:58:45 +0100

    create model for answered question

--- src/main/java/com/codeWithMerald/RoyalSealLearningSystem/models/test/Question.java
+++ src/main/java/com/codeWithMerald/RoyalSealLearningSystem/models/test/Question.java
@@ -1,6 +1,7 @@
 package com.codeWithMerald.RoyalSealLearningSystem.models.test;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -10,6 +11,7 @@ import java.util.List;
 @Entity
 @Table(name = "question")
 @Data
+@AllArgsConstructor
 public class Question {
 
     @Id
