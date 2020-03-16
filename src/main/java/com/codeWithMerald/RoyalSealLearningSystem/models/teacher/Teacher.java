2e56ac77b123c4477ae77cf9a81dc382b2d39541
Meralds-World <grace.chidumaga@gmail.com>
2020-03-16 13:12:16 +0100

    create teacher model

--- src/main/java/com/codeWithMerald/RoyalSealLearningSystem/models/teacher/Teacher.java
+++ src/main/java/com/codeWithMerald/RoyalSealLearningSystem/models/teacher/Teacher.java
@@ -1,4 +1,53 @@
 package com.codeWithMerald.RoyalSealLearningSystem.models.teacher;
 
-public class Teacher {
+import com.codeWithMerald.RoyalSealLearningSystem.models.course.Course;
+import com.codeWithMerald.RoyalSealLearningSystem.models.department.Department;
+import com.codeWithMerald.RoyalSealLearningSystem.models.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "teacher")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Teacher extends User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long teacherId;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+    @ManyToMany(mappedBy = "teachers")
+    private Set<Course> courses;
+
+    public Long getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(Long teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public Set<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(Set<Course> courses) {
+        this.courses = courses;
+    }
 }
