0656096c1c9b170df708cb2632eae6ca27b01201
Meralds-World <grace.chidumaga@gmail.com>
2020-03-16 10:22:29 +0100

    create assignment model

--- src/main/java/com/codeWithMerald/RoyalSealLearningSystem/models/course/Assignment.java
+++ src/main/java/com/codeWithMerald/RoyalSealLearningSystem/models/course/Assignment.java
@@ -1,4 +1,104 @@
 package com.codeWithMerald.RoyalSealLearningSystem.models.course;
 
+import com.codeWithMerald.RoyalSealLearningSystem.models.student.StudentAssignment;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Set;
+
+@Entity
+@Table(name = "assignments")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Assignment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long assignmentId;
+
+    private String name;
+    private String description;
+    private Date startDate;
+    private Date endDate;
+    private String file;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "student")
+    private Set<StudentAssignment> studentAssignments;
+
+    public Long getAssignmentId() {
+        return assignmentId;
+    }
+
+    public void setAssignmentId(Long assignmentId) {
+        this.assignmentId = assignmentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getFile() {
+        return file;
+    }
+
+    public void setFile(String file) {
+        this.file = file;
+    }
+
+    @JsonIgnore
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    @JsonIgnore
+    public Set<StudentAssignment> getStudentAssignments() {
+        return studentAssignments;
+    }
+
+    public void setStudentAssignments(Set<StudentAssignment> studentAssignments) {
+        this.studentAssignments = studentAssignments;
+    }
 }
