922259f0db65d898a95a4ff0c057ff07fdd03cb3
Meralds-World <grace.chidumaga@gmail.com>
2020-03-18 17:55:59 +0100

    create courses controller

--- src/main/java/com/codeWithMerald/RoyalSealLearningSystem/repositories/course/CourseRepository.java
+++ src/main/java/com/codeWithMerald/RoyalSealLearningSystem/repositories/course/CourseRepository.java
@@ -1,7 +1,9 @@
 package com.codeWithMerald.RoyalSealLearningSystem.repositories.course;
 
 import com.codeWithMerald.RoyalSealLearningSystem.models.course.Course;
+import com.codeWithMerald.RoyalSealLearningSystem.models.user.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
